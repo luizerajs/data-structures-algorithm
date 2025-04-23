967a0fac22067e97676194aa3069dcf605456879
Luiz Chaves <luizchaves97@gmail.com>
2025-04-23 12:17:03 -0300

    feat: add queue data structure

---
+++ 02-queue.js
@@ -0,0 +1,53 @@
+class Queue {
+  constructor() {
+    this.list = [];
+  }
+
+  enqueue(item) {
+    // O(1)
+    this.list.push(item);
+  }
+
+  dequeue() {
+    // O(n)
+    if (this.isEmpty()) {
+      throw new Error("This queue is empty.");
+    }
+
+    return this.list.shift();
+  }
+
+  peek() {
+    // O(1)
+    if (this.isEmpty()) {
+      throw new Error("This queue is empty.");
+    }
+
+    return this.list[0];
+  }
+
+  isEmpty() {
+    // O(1)
+    return this.list.length === 0;
+  }
+
+  size() {
+    // O(1)
+    return this.list.length;
+  }
+
+  print() {
+    // O(n)
+    console.log(this.list.join(" -> "));
+  }
+}
+
+// Example usage:
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.print();
+console.log(queue.dequeue());
+console.log(queue.peek());
+console.log(queue.size());
