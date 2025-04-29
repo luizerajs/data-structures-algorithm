6b183abfa98895192c2d318bf0e2bd7cc93d8551
Luiz Chaves <luizchaves97@gmail.com>
2025-04-28 22:11:33 -0300

    feat: improve description of the solution

---  exercises/arrays/contains-duplicate.js
+++  exercises/arrays/contains-duplicate.js
@@ -1,3 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+
+    problem: find if the array has a duplicate number
+
+    constraints: 
+    - input: nums > 0 e nums < 10ˆ5 (100.000.000)
+    - input: 10^9 <= nums[i] <= 10ˆ9 
+
+    output:
+    - true if any value appears at least twice
+    - false if every element is distinct
+
+    examples: 
+    - [1,2,3,4] expect(false)
+    - [1,3,4,1] expect(true)
+
+    Questions:
+    - O input é mutável? X
+    - Os dados estão ordenados? V
+    - O input pode estar vazio? V 
+
+    Solução:
+    - Hash Table (Map) pra armazenar os valores visitados.
+    - Se em algum momento eu encontrar o elemento nos visitados
+    - Então eu retorno true. Caso contrário false
+ */
 var containsDuplicate = function (nums) {
   const visited = new Set();
 
@@ -11,6 +39,3 @@ var containsDuplicate = function (nums) {
 
   return false;
 };
-
-console.log(containsDuplicate([1, 2, 3, 4])); // expect: false
-console.log(containsDuplicate([1, 2, 3, 1])); // expect: true
